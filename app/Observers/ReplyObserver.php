c93ff1a24c9ac085d3efb897db50e49dadf86896
vagrant <vagrant@homestead>
2021-10-22 03:26:51 +0000

    评论发表

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use Auth;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,7 +12,14 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->last_reply_user_id = Auth::id();
+        $reply->topic->save();
     }
 
     public function updating(Reply $reply)
